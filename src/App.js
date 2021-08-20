587144846c5ec3e06aa7b3fdd8ec40f4d00a44da
nancy100194 <83443374+nancy100194@users.noreply.github.com>
2021-08-20 12:46:19 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,147 @@
+import logo from './logo.svg';
+import './App.css';
+import Card from "./Card";
+function App() {
+  let details = [
+    {
+      packageName : "FREE",
+      price : 0,
+      currency : "$",
+      period : "monthly",
+      features : [
+        {
+          name : "Single User",
+          isEnabled : true
+        },
+        {
+          name : "5GB Storage",
+          isEnabled : true
+        },
+        {
+          name : "Unlimited Public Projects",
+          isEnabled : true
+        },
+        {
+          name : "Community Access",
+          isEnabled : false
+        },
+        {
+          name : "Unlimited Private Projects",
+          isEnabled : false
+        },
+        {
+          name : "Dedicated Phone Support",
+          isEnabled : false
+        },
+        {
+          name : "Free Subdomain",
+          isEnabled : false
+        },
+        {
+          name : "Monthly Status Reports",
+          isEnabled : false
+        }
+      ]
+    },
+    {
+      packageName : "PLUS",
+      price : 9,
+      currency : "$",
+      period : "monthly",
+      features : [
+        {
+          name : "5 Users",
+          isEnabled : true,
+          isBold : true
+        },
+        {
+          name : "50GB Storage",
+          isEnabled : true
+        },
+        {
+          name : "Unlimited Public Projects",
+          isEnabled : true
+        },
+        {
+          name : "Community Access",
+          isEnabled : true
+        },
+        {
+          name : "Unlimited Private Projects",
+          isEnabled : true
+        },
+        {
+          name : "Dedicated Phone Support",
+          isEnabled : true
+        },
+        {
+          name : "Free Subdomain",
+          isEnabled : true
+        },
+        {
+          name : "Monthly Status Reports",
+          isEnabled : false
+        },
+      ]
+    },
+    {
+      packageName : "PRO",
+      price : 49,
+      currency : "$",
+      period : "yearly",
+      features : [
+        {
+          name : "Unlimited Users",
+          isEnabled : true
+        },
+        {
+          name : "150GB Storage",
+          isEnabled : true
+        },
+        {
+          name : "Unlimited Public Projects",
+          isEnabled : true
+        },
+        {
+          name : "Community Access",
+          isEnabled : true
+        },
+        {
+          name : "Unlimited Private Projects",
+          isEnabled : true
+        },
+        {
+          name : "Dedicated Phone Support",
+          isEnabled : true
+        },
+        {
+          name : "Free Subdomain",
+          isEnabled : true
+        },
+        {
+          name : "Monthly Status Reports",
+          isEnabled : true
+        },
+      ]
+    }
+  ]
+
+  let onSubscribe = (plan) => {
+    alert(`Hey ${plan} plan is subscribed`)
+  }
+  return (
+    <section class="pricing py-5">
+      <div class="container">
+        <div class="row">
+          {
+            details.map((card) => {
+                return <Card data={card} handleSubscribe={onSubscribe}></Card>
+            })
+          }
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default App;
